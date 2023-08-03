a1e3576db1a2bb6812616cf74002c1b331230c9f
10soa <soaornella5@gmail.com>
2023-08-03 21:31:49 +0300

    page accueil

---
+++ app/src/main/java/com/example/andao_apk/Categorie/CategorieClass.java
@@ -0,0 +1,61 @@
+package com.example.andao_apk.Categorie;
+
+public class CategorieClass {
+    private String lien;
+    private String _id;
+    private String categorie;
+
+    private int image;
+
+    public CategorieClass(){
+
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public CategorieClass(String id, String lien, String categorie) {
+        this.lien = lien;
+        this._id = id;
+        this.categorie = categorie;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getLien() {
+        return lien;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public void setLien(String lien) {
+        this.lien = lien;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public CategorieClass(String lien, String categorie) {
+        this.lien = lien;
+        this.categorie = categorie;
+    }
+
+    public CategorieClass(int image, String categorie) {
+        this.image = image;
+        this.categorie = categorie;
+    }
+}
