b6455bd14a3f04b520fa57263b5ad097f7e2858f
mathieu29nick <96386431+mathieu29nick@users.noreply.github.com>
2023-08-05 16:15:18 +0300

    [Android] Correction Navigation

--- app/src/main/java/com/example/andao_apk/Utilisateur/ficheUtilisateur.java
+++ app/src/main/java/com/example/andao_apk/Utilisateur/ficheUtilisateur.java
@@ -1,16 +1,40 @@
 package com.example.andao_apk.Utilisateur;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.example.andao_apk.MainActivity;
 import com.example.andao_apk.R;
 
 public class ficheUtilisateur extends AppCompatActivity {
 
+
+    private ImageView retour;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fiche_utilisateur);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        retour=findViewById(R.id.retour_profil);
+        retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ficheUtilisateur.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
     }
 }
